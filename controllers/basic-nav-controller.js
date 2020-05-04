7557654f261f1322a35a71a8cb9eae39bae508f6
Lng123 <louisng1@hotmail.com>
2020-05-03 19:10:42 -0700

    transit tooltip bus information added

--- controllers/basic-nav-controller.js
+++ controllers/basic-nav-controller.js
@@ -1,3 +1,6 @@
-exports.getMap = (req, res) => {
-    res.render('basic-nav', { basicNavCSS: true });
+let basicModel = require("../models/basic_model");
+exports.getMap = async (req, res) => {
+    let transitData = await basicModel.getTransitInfo();
+    busData = transitData[0]
+    res.render('basic-nav', { basicNavCSS: true, transit: busData });
 };
