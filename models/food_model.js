803fb1940b6ce01c6c0186317781b4b16cbb681b
Wilburt Herrera <wilburt_h@hotmail.com>
2020-05-05 22:22:41 -0700

    included microwave, social, and study areas to db and updated other-key-places.hbs

--- models/food_model.js
+++
@@ -1,8 +0,0 @@
-const db = require("../util/database");
-const mysql = require("mysql2");
-
-exports.getFoodInfo = () => {
-  let sql = `SELECT * FROM foodServices`;
-
-  return db.execute(sql);
-};
