805d6feaa31f06cbf826195f1c069f3e28a11c20
Lng123 <louisng1@hotmail.com>
2020-05-13 17:33:41 -0700

    loading wheel basic nav

--- public/js/highlight.js
+++ public/js/highlight.js
@@ -282,12 +282,18 @@ function showDetailsParking(description){
     document.getElementById("details_box").style.overflow= "scroll"
 }
 
+function loading() {
+    return '<div id="loading" class="d-flex justify-content-center align-items-center" style="height: 100%">'
+     + '<div class="spinner-border" style="width: 6rem; height: 6rem;" role="status">'
+    +  '<span class="sr-only">Loading...</span></div></div>';
+}
+
 var transit = false;
 var busData = [];
 function selectTransit() {  
     showBasicOverlay("/media/bus_map.png");
-    console.log(transit);
     if (transit == false) {
+        $(loading()).insertAfter(".campus-map");
         fetch("/getTransit")
         .then(res => res.text())
         .then(function (data) {
@@ -302,6 +308,7 @@ function selectTransit() {
             })
             busOverlay();
             transit = true;
+            $("#loading").remove()
         })
         .catch(function (error) {
             console.log(error);
@@ -309,6 +316,7 @@ function selectTransit() {
     } else {
         busOverlay();
     }
+    $(loading()).insertAfter(".campus-map");
     fetch("/getTransitDesc")
     .then(res => res.text())
     .then(function (data) {
@@ -320,6 +328,7 @@ function selectTransit() {
     .catch(function (error) {
         console.log(error);
     });
+    $("#loading").remove()
 }
 
 function busOverlay() {
